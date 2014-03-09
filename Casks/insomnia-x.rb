6f341da9ff98e66b5189d8ca631b94863ef710da
Kenta Murata <mrkn@cookpad.com>
2014-03-09 16:37:40 +0900

    Add Casks/insomnia-x.rb

---
+++ Casks/insomnia-x.rb
@@ -0,0 +1,7 @@
+class InsomniaX < Cask
+  url 'http://insomniax.semaja2.net/InsomniaX-2.1.3.tgz'
+  homepage 'http://semaja2.net/archives/258'
+  version '2.1.3'
+  sha1 '76978e1044b9aa4d9b212d675f7ec2c87674c9e5'
+  link 'InsomniaX.app'
+end

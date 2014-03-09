e347123186146bfbf2d5bae25dd905739d472ad9
Kenta Murata <mrkn@mrkn.jp>
2014-03-10 08:19:47 +0900

    Add QuartzClocks 1.3.6

---
+++ Casks/quartz-clocks.rb
@@ -0,0 +1,8 @@
+class QuartzClocks < Cask
+  url 'https://www.macupdate.com/download/23561/quartzclocks.dmg'
+  homepage 'https://www.macupdate.com/app/mac/23561/quartzclocks/'
+  version '1.3.6'
+  sha256 'c0442529962e9102aeed010497c3ac26e229df84a6c315ec17cef251d032e7e9'
+  link 'QuartzClocks.app'
+  font 'Recommended Fonts/Casual'
+end

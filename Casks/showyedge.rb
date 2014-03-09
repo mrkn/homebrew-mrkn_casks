607b4d46457f9209d6a9da057435bcd5e4bb2266
Kenta Murata <mrkn@mrkn.jp>
2014-03-10 03:08:45 +0900

    Add ShowyEdge

---
+++ Casks/showyedge.rb
@@ -0,0 +1,7 @@
+class ShowyEdge < Cask
+  url 'https://pqrs.org/macosx/ShowyEdge/files/ShowyEdge-1.7.0.dmg'
+  homepage 'https://pqrs.org/macosx/ShowyEdge/index.html.ja'
+  version '1.7.0'
+  sha1 'e40c611a843c2a21864fc0f6efe8e1fab1d78dd3'
+  link 'ShowyEdge.app'
+end

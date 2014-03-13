e0678cc07bb1c481e93eadd86bfcdb4308bd548e
Kenta Murata <mrkn@mrkn.jp>
2014-03-14 07:37:52 +0900

    Add DTerm.app

---
+++ Casks/dterm.rb
@@ -0,0 +1,7 @@
+class Dterm < Cask
+  url 'http://files.decimus.net/DTerm/DTerm.zip'
+  homepage 'http://decimus.net/dterm'
+  version '1.6'
+  sha256 '34292f3450567f2964288998b3fdda0f85f49a36845457c46a74974e41c234ac'
+  link 'DTerm.app'
+end
